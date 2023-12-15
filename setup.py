efcb57a145a0e0e741fc44ad815ce417ed38237f
Yash-ingle-9021 <codebyyash0803@gmail.com>
2023-12-15 00:49:00 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Cat-Dog",
+    version="0.0.1",
+    author="Yash Ingle",
+    author_email="codebyyash0803@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
